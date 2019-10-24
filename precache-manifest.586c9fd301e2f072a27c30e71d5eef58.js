1522de888ca950318011796abe446c0b1c79020d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-24 15:37:24 +0000

    Deploy gpn-prototypes/ui-kit to github.com/gpn-prototypes/ui-kit.git:gh-pages

---
+++ precache-manifest.586c9fd301e2f072a27c30e71d5eef58.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c24a4fba67a41e42de621925b65119",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d57419816b47c1df91f7.bundle.js"
+  },
+  {
+    "url": "runtime~main.d57419816b47c1df91f7.bundle.js"
+  },
+  {
+    "url": "vendors~main.d57419816b47c1df91f7.bundle.js"
+  }
+]);
